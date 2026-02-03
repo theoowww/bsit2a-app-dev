666eef6e3711f28eef5b67c0e7512c1c080ab236
theoowww <barramedaaries98@gmail.com>
2026-02-03 10:24:23 +0800

    added layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,41 @@
 <template>
   <v-app>
+     <v-navigation-drawer v-model="drawer"  :rail="rail" permanent>
+
+        <template v-slot:prepend>
+          <v-list-item
+            lines="two"
+            prepend-avatar="https://randomuser.me/api/portraits/women/81.jpg"
+            subtitle="Logged in"
+            title="Jane Smith"
+          ></v-list-item>
+        </template>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+            <v-app-bar :elevation="2">
+  <template v-slot:prepend>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+  </template>
+
+  <v-app-bar-title>Application Bar</v-app-bar-title>
+</v-app-bar>
+<v-main>
+  <v-container>
     <slot />
+    </v-container>
+    </v-main>
   </v-app>
 </template>
 
-<script lang="ts" setup>
-
+<script setup>
+const drawer = ref();
 </script>
 
 <style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <v-btn>Submit</v-btn>
+    
+    <v-row>
+      <v-col cols="12" md="3">
+        <v-card height="90" color="primary"></v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="90" color="red"></v-card>
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-card height="90" color="pink"></v-card>
+      </v-col>
+      <v-col cols="12"md="3">
+        <v-card height="90" color="orange"></v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
